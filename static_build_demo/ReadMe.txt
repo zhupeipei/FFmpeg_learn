a2b285ab864d955a61b5046b841644019fcf48fe
peipei.zhu <peipei.zhu@ximalaya.com>
2022-08-07 02:11:50 +0800

    init project

---
+++ static_build_demo/ReadMe.txt
@@ -0,0 +1,9 @@
+ffmpeg源码拉下来后执行build.sh脚本
+
+export LDFLAGS="-L/usr/local/opt/openssl/lib"
+export CPPFLAGS="-I/usr/local/opt/openssl/include"
+
+./configure --prefix=/Users/xmly/audio_video/FFmpeg_compile_local_mac/build_local_mac \
+--disable-static \
+--enable-shared
+
---
+++ static_build_demo/build.sh
@@ -0,0 +1,29 @@
+export LDFLAGS="-L/usr/local/opt/openssl/lib"
+export CPPFLAGS="-I/usr/local/opt/openssl/include"
+
+./configure --prefix=/Users/xmly/audio_video/FFmpeg_compile_local_mac/build_local_mac \
+--disable-static \
+--enable-shared
+#./configure --prefix=/Users/xmly/audio_video/FFmpeg_compile_local_mac/build_local_mac \
+#--enable-gpl \
+#--enable-libx264
+#--enable-openssl \
+#--enable-version3 \
+#--enable-nonfree \
+#--enable-postproc \
+#--enable-libass \
+#--enable-libfreetype \
+#--enable-libmp3lame \
+#--enable-libopencore-amrnb \
+#--enable-libopencore-amrwb \
+#--enable-libopenjpeg \
+#--enable-libopus \
+#--enable-libspeex \
+#--enable-libtheora \
+#--enable-libvorbis \
+#--enable-libvpx \
+#--enable-libxvid
+#--enable-libfdk-aac \
+#--disable-static \
+#--enable-shared
+#--enable-libcelt \
