d96d4e56e086636261d02fca5c87ae174591f3d4
peipei.zhu <peipei.zhu@ximalaya.com>
2022-09-02 17:32:06 +0800

    [feat] init ffmpeg android

---
+++ FFmpegAndroidLearn/app/src/main/cpp/include/x264_config.h
@@ -0,0 +1,8 @@
+#define X264_GPL           1
+#define X264_INTERLACED    1
+#define X264_BIT_DEPTH     0
+#define X264_CHROMA_FORMAT 0
+#define X264_REV 3096
+#define X264_REV_DIFF 0
+#define X264_VERSION " r3096M f7074e1"
+#define X264_POINTVER "0.164.3096M f7074e1"
--- FFmpegAndroidLearn/app/src/main/cpp/native-lib.cpp
+++ FFmpegAndroidLearn/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,29 @@
 #include <jni.h>
 #include <string>
+#include "android/log.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "libavcodec/avcodec.h"
+#include "libavutil/avutil.h"
+#include "libavutil/ffversion.h"
+#ifdef __cplusplus
+}
+#endif
+
+#define LOGV(FMT, ...) __android_log_print(ANDROID_LOG_VERBOSE, "nativeLog", FMT, __VA_ARGS__)
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_aire_ffmpegLearn_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+    LOGV("FFMPEG VERSION: %s", FFMPEG_VERSION);
+    LOGV("AVCODEC VERSION: %d", avcodec_version());
+    const AVCodec *codec = avcodec_find_encoder_by_name("libx264");
+    if (codec) {
+        LOGV("FOUND X264, name: %s, type: %d", codec->name, codec->type);
+    }
     return env->NewStringUTF(hello.c_str());
 }
